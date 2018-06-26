6460565fe146f9ab6b2df16267cf83abf7e910f0
SEOKWEON JUNG <minimus@seoul.ac.kr>
2018-06-26 17:48:46 +0900

    "time showing page"

--- DreamCompanion/DreamCompanion/urls.py
+++ DreamCompanion/DreamCompanion/urls.py
@@ -16,28 +16,8 @@ import app.views
 # admin.autodiscover()
 
 urlpatterns = [
-    # Examples:
-    url(r'^$', app.views.home, name='home'),
-    url(r'^contact$', app.views.contact, name='contact'),
-    url(r'^about', app.views.about, name='about'),
-    url(r'^login/$',
-        django.contrib.auth.views.login,
-        {
-            'template_name': 'app/login.html',
-            'authentication_form': app.forms.BootstrapAuthenticationForm,
-            'extra_context':
-            {
-                'title': 'Log in',
-                'year': datetime.now().year,
-            }
-        },
-        name='login'),
-    url(r'^logout$',
-        django.contrib.auth.views.logout,
-        {
-            'next_page': '/',
-        },
-        name='logout'),
+    url(r'^$', HelloDjangoApp.views.index, name = 'index'),
+    url(r'^home$', HelloDjangoApp.views.index, name = 'home'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
